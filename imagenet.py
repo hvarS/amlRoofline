a43997bb30ce3f96097c13d37a908a341d16c16d
Harshvardhan Srivastava <srivastavahv@gmail.com>
2024-03-28 18:24:44 -0400

    mnet

--- imagenet.py
+++ imagenet.py
@@ -234,7 +234,7 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = RNetModel()
+    model = MNetModel()
     model = model.cuda()
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
