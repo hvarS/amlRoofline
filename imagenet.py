092d09325d9817c3b66095746cf672486921fa8e
Harshvardhan Srivastava <srivastavahv@gmail.com>
2024-03-29 01:31:32 -0400

    rnet

--- imagenet.py
+++ imagenet.py
@@ -240,7 +240,7 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = CNNModel()
+    model = RNetModel()
     
 
     # Create the FlopCountAnalysis and ActivationCountAnalysis objects
