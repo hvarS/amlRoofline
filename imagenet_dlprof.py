9558d9e2a6ebea84e013d61620309e60691f5fb0
Harshvardhan Srivastava <srivastavahv@gmail.com>
2024-03-28 21:00:12 -0400

    reduced batch size

--- imagenet_dlprof.py
+++ imagenet_dlprof.py
@@ -165,7 +165,7 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('data', metavar='DIR', nargs='?', default='imagenet',
                     help='path to dataset (default: imagenet)')
-    parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
@@ -235,7 +235,7 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = RNetModel()
+    model = MNetModel()
     model = model.cuda()
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
