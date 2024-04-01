180bd8d87198be71a08feff53b82778d225eb737
Harshvardhan Srivastava <srivastavahv@gmail.com>
2024-03-31 21:24:01 -0400

    print only once

--- imagenet.py
+++ imagenet.py
@@ -124,9 +124,9 @@ def train(args, model, device, train_loader, optimizer, epoch):
         with torch.profiler.profile(
         activities= [torch.profiler.ProfilerActivity.CPU],
         schedule=torch.profiler.schedule(
-            wait = 1,
-            warmup=0,
-            active=3),
+            wait = 0,
+            warmup=1,
+            active=4),
         with_stack = True,
         on_trace_ready=torch.profiler.tensorboard_trace_handler(f'./log/{args.m}_{torch.cuda.get_device_name()}'),
         profile_memory=True, 
@@ -150,8 +150,9 @@ def train(args, model, device, train_loader, optimizer, epoch):
 
             if args.dry_run:
                 break
+    if(epoch == int(args.epochs) - 1):
+        print(p.key_averages().table())
     
-    print(p.key_averages().table())
         
 
 def test(model, device, test_loader):
