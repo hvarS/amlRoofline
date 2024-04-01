339d6e11da8faeebffc9db21ffe2413c9d6fc2dc
Harshvardhan Srivastava <srivastavahv@gmail.com>
2024-03-31 21:35:00 -0400

    removed sorting

--- imagenet.py
+++ imagenet.py
@@ -147,7 +147,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
             if args.dry_run:
                 break
     if(epoch == int(args.epochs) - 1):
-        print(p.key_averages(group_by_stack_n=1).table(sort_by="cpu_time_total"))
+        print(p.key_averages(group_by_stack_n=1))
     
         
 
